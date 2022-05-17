767117c8002c5e58d3edecf67e9d0ed45dd68bf6
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-17 20:28:00 -0300

    Initial Commit

---
+++ Shared/Habit_App.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_App.swift
+//  Shared
+//
+//  Created by Antonio Vuono on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Habit_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
