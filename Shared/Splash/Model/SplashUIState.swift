f28cca00cd0c8ae9d90fc202dd47d21860ad69fa
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-17 21:32:50 -0300

    Splash screen and error started configured

--- Shared/Splash/Model/SplashUIState.swift
+++ Shared/Splash/Model/SplashUIState.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+enum SplashUIState {
+    case loading
+    case goToSignInScreen
+    case goToHomeScreen
+    case error(String)
+    
+}
--- Shared/Splash/View/SplashView.swift
+++ Shared/Splash/View/SplashView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State var state: SplashUIState = .loading
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch state {
+        case .loading:
+             loadingView()
+        case .goToSignInScreen:
+             Text("Carregar a tela de login ")
+        case .goToHomeScreen:
+             Text("Carregar a homepage")
+        case .error(let msg):
+            loadingView(error: msg)
+        }
+    }
+}
+
+extension SplashView {
+    func loadingView(error: String? = nil) -> some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
+            
+            
+            if let error = error {
+                Text("")
+                    .alert(isPresented: .constant(true)) {
+                        Alert(title: Text("Habit+"), message: Text(error), dismissButton: .default(Text("Ok")) {
+                            // faz algo quando some o alerta!
+                        })
+                    }
+            }
+        }
     }
 }
 
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
-        SplashView()
+        SplashView(state: .error("Não foi possível conectar ao servidor!"))
+//        SplashView()
     }
 }
